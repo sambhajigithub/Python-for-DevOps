d2d7e1b45ccccbfbbd4ad39a982c8e50a33c7c57
Sambhaji Biradar <110982296+sambhajigithub@users.noreply.github.com>
2023-11-15 18:43:54 +0530

    .

---
+++ Ask the user for their name and their age. Add 1 to their age.py
@@ -0,0 +1,4 @@
+name = input ("what is your name? ")
+age = int(input("how old are you?"))
+newAge = age + 1
+print(name,"next birthday you will be", newAge)
---
+++ price of the bill.py
@@ -0,0 +1,4 @@
+bill = int(input("What is the total cost of the bill? "))
+people = int(input("how many pepole are there? "))
+each = bill/people
+print ("Each person should pay ₹", each)
